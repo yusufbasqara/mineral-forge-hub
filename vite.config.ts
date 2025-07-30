141c33b67c16efd5f16cb90ee4185ea1f73ba9a7
yusufbasqara <yusufbasqara@gmail.com>
2025-07-31 00:24:04 +0700

    set ViteConfig

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: 'gi/mineral-forge-hub/',
+  base: '/mineral-forge-hub/',
   server: {
     host: "::",
     port: 8080,
